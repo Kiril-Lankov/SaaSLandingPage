1d9dc13117551a9419cd8c2e080b877373bdce20
Kiril-Lankov <k1r3t0@gmail.com>
2025-03-11 18:45:03 +0200

    Update Features.jsx

---
+++ my-project/src/sections/Features.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Features = () => {
+    return (
+        <section>
+            <Element name='features'>
+                <div className='container'>
+                    <div className='relative flex'></div>
+                </div>
+            </Element>
+        </section>
+    )
+}
+
+export default Features
