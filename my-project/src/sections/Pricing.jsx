373a806d349adc01f62e5629f7b380b3aa7e5e3f
Kiril-Lankov <k1r3t0@gmail.com>
2025-03-12 17:47:01 +0200

    Update Pricing.jsx

--- my-project/src/sections/Pricing.jsx
+++ my-project/src/sections/Pricing.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import {Element} from 'react-scroll'
 
 const Pricing = () => {
     return (
-        <section></section>
+        <section>
+            <Element name='pricing'>
+                <div className='container'>
+                    <div className='max-w-960 border-2 border-red-500'>
+                        <h3 className='h3 max-lg:h4 max-md:h5 z-3 relative mx-auto mb-14 max-w-lg text-center text-p4 max-md:mb-11 mx-sm:max-w-sm'>
+                            Flexible pricing for teams of all sizes
+                        </h3>
+                        <div>
+                            <button>Montly</button>
+                            <button>Annual</button>
+                        </div>
+                    </div>
+                </div>
+            </Element>
+        </section>
     )
 }
 
