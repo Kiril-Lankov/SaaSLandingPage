06b5b8e359a1131d3d3a5fda47309b820f6c078b
Kiril-Lankov <k1r3t0@gmail.com>
2025-03-12 22:05:26 +0200

    Update Download.jsx

--- my-project/src/sections/Download.jsx
+++ my-project/src/sections/Download.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { Element } from 'react-scroll'
 
 const Download = () => {
     return (
-        <div></div>
+        <section>
+            <Element name='download' className='g7 relative pb-32 pt-24 max-lg:pb-24 max-md:py-16'>
+                <div className='container'>
+                    <div className='flex items-center'>
+                        <div className='relative mr-6 flex-540 max-xl:flex-280 max-lg:flex-256 max-md:flex-100'>
+                            <div className='mb-10'>
+                                <img 
+                                src='/images/xora.svg' 
+                                width={160} 
+                                height={55} 
+                                alt='xora' />
+                            </div>
+                            <p className='body-1 mb-10 max-w-md'>
+                                Try it now for free on iOS, Android, PC, Web - whatever your flavour
+                                , we've got you covered
+                            </p>
+                            <ul className='flex flex-wrap items-center gap-6'>
+                                {links.map}
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </Element>
+        </section>
     )
 }
 
